117c987198e9a24cb4d94a465aac3224922ad1f6
Rosalia Padoin Oliveira <rosalia@rosalia-G7-7588>
2022-04-12 16:10:38 -0300

    desafio 10 e 11

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+SELECT
+    cancao AS nome_musica,
+    CASE
+        WHEN cancao LIKE '%Streets' THEN REPLACE(cancao, 'Streets', 'Code Review')
+        WHEN cancao LIKE '%Her Own' THEN REPLACE(cancao, 'Her Own', 'Trybe')
+        WHEN cancao LIKE '%Inner Fire' THEN REPLACE(cancao, 'Inner Fire', 'Project')
+        WHEN cancao LIKE '%Silly' THEN REPLACE(cancao, 'Silly', 'Nice')
+        WHEN cancao LIKE '%Circus' THEN REPLACE(cancao, 'Circus', 'Pull Request')
+    END AS 'novo_nome'
+FROM SpotifyClone.cancoes
+GROUP BY nome_musica
+HAVING nome_musica <> novo_nome
+ORDER BY novo_nome, nome_musica;
