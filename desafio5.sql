d74369482f8c100e42eb82aafaa79b1d183d221f
Rosalia Padoin Oliveira <rosalia@rosalia-G7-7588>
2022-04-12 13:37:58 -0300

    desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT can.cancao AS cancao, COUNT(his.cancao_id) AS reproducoes
+FROM SpotifyClone.cancoes AS can
+INNER JOIN SpotifyClone.historico AS his
+ON his.cancao_id = can.cancao_id
+GROUP BY can.cancao
+ORDER BY reproducoes DESC, can.cancao
+LIMIT 2;
