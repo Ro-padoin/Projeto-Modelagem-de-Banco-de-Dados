d15489cf22a3bdb1da4536d2be34f48c12a7d7cc
Rosalia Padoin Oliveira <rosalia@rosalia-G7-7588>
2022-04-12 12:53:44 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,6 @@
+SELECT usu.usuario AS usuario,
+IF (MAX(YEAR(his.data_reproducao)) = 2021, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+FROM SpotifyClone.usuarios AS usu
+INNER JOIN SpotifyClone.historico AS his
+ON usu.usuario_id = his.usuario_id
+GROUP BY usu.usuario;
