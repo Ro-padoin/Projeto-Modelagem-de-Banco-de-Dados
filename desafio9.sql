928a87e222d11e3f6e0a348a0b0d26b5ed8192f7
Rosalia Padoin Oliveira <rosalia@rosalia-G7-7588>
2022-04-12 15:22:17 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(his.usuario_id) AS quantidade_musicas_no_historico
+FROM SpotifyClone.historico AS his
+INNER JOIN SpotifyClone.usuarios AS usu
+ON his.usuario_id = usu.usuario_id AND usu.usuario = 'Bill';
