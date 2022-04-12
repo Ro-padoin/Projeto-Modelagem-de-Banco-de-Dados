d89cd935c4e8c40fa37b052ddef43fcdfe2155de
Rosalia Padoin Oliveira <rosalia@rosalia-G7-7588>
2022-04-12 15:11:28 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,5 @@
+SELECT art.artista AS artista, alb.album AS album
+FROM SpotifyClone.artistas AS art
+INNER JOIN SpotifyClone.albuns AS alb
+ON art.artista_id = alb.artista_id 
+AND art.artista = 'Walter Phoenix';
