531a8fe113b8b2487e916d400caae7bae29dc2b8
Rosalia Padoin Oliveira <rosalia@rosalia-G7-7588>
2022-04-12 14:42:02 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+SELECT ROUND(MIN(pla.valor), 2) AS faturamento_minimo, 
+ROUND(MAX(pla.valor), 2) AS faturamento_maximo,
+ROUND(AVG(pla.valor), 2) AS faturamento_medio,
+ROUND(SUM(pla.valor), 2) AS faturamento_total
+FROM SpotifyClone.planos AS pla
+INNER JOIN SpotifyClone.usuarios AS usu
+ON pla.plano_id = usu.plano_id;
